672b709375aca57c3da7e21c91f46d969b1224b2
Mina Shi <mshi0002@corp.bank.nzpfs.co.nz>
2021-06-15 09:39:09 +1200

    add script

---
+++ testgit.ps1
@@ -0,0 +1,11 @@
+﻿$Headers = @{
+Accept = "application/vnd.github.v3+json"
+}
+$GitUrl=git remote show origin -n | select-string -Pattern "Fetch URL"
+$RepoName=$GitUrl.ToString().Split("/")[-2]
+$RepoDetails=$GitUrl.ToString().Split("/")[-1].Split(".")[0]
+$Comments=(Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/repos/$RepoName/$RepoDetails/pulls).body
+if(!$Comments.Contains("mment")){
+        throw "Not meet our criteria"
+}
+Write-Output "PR comment check done"
